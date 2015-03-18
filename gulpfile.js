8ebcb6f10c0ebc17a2d6879d89fc7fa2df36eb27
Andre Nanninga <andre.nanninga@buyways.nl>
2015-03-18 16:44:16 +0100

    ugly working version

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,15 @@
 'use strict';
 
-var gulp       = require('gulp');
-var connect    = require('gulp-connect');
-var browserify = require('gulp-browserify');
-var plumber    = require('gulp-plumber');
-var ignore     = require('gulp-ignore');
+var _           = require('underscore');
+var del         = require('del');
+var gulp        = require('gulp');
+var connect     = require('gulp-connect');
+var browserify  = require('gulp-browserify');
+var jsoncombine = require('gulp-jsoncombine');
+var plumber     = require('gulp-plumber');
+var ignore      = require('gulp-ignore');
+var run         = require('gulp-run');
+var map         = require('vinyl-map');
 
 gulp.task('webserver', function() {
   connect.server({
@@ -28,16 +33,80 @@ gulp.task('copy', function() {
     .pipe(gulp.dest('./build'));
 });
 
-gulp.task('assets', function() {
-  gulp.src('./assets/**/*', { base: '.' })
-    .pipe(plumber())
-    .pipe(gulp.dest('./build'));
-});
-
 gulp.task('watch', function() {
   gulp.watch('./src/js/*', ['browserify']);
   gulp.watch('./src/**/*', ['copy']);
 });
 
-gulp.task('build', ['browserify', 'copy', 'assets']);
+gulp.task('build:map', ['verify:ogr2ogr', 'verify:topojson'], function() {
+  del('./build/assets/world/*.json');
+
+  gulp.src('.')
+    .pipe(plumber())
+    .pipe(run([
+      'ogr2ogr',
+      '-f GeoJSON',
+      '-where "adm0_a3 != \'ATA\'"',
+      './build/assets/world/countries.json',
+      './assets/world/shapefiles/ne_110m/ne_110m_admin_0_countries.shp',
+
+      '&&',
+      
+      'topojson',
+      '-o ./build/assets/world/world.json',
+      '--properties name=name',
+      '--properties cca3=adm0_a3',
+      './build/assets/world/countries.json'
+    ].join(' ')));
+
+  gulp.src(['build/assets/world/world.json', './assets/world/countries.json'])
+    .pipe(jsoncombine('data.json', function(data) {
+      var geometries = data.world.objects.countries.geometries;
+      var countries = data.countries;
+
+      geometries = _.map(geometries, function(geometry) {
+        var country = _.findWhere(countries, { 'cca3': geometry.properties.cca3 });
+
+        if(country) {
+          geometry.properties.borders = country.borders;
+        }
+        else {
+          geometry.properties.borders = [];
+        }
+
+        if(geometry.properties.cca3 === 'SAH') {
+          geometry.properties.borders = ['DZA', 'MRT', 'MAR'];
+        }
+
+        return geometry;
+      });
+
+      return new Buffer(JSON.stringify(data.world));
+    }))
+    .pipe(gulp.dest('./build/assets/world'));
+});
+
+gulp.task('verify:ogr2ogr', function() {
+  gulp.src('.')
+    .pipe(plumber())
+    .pipe(run('which ogr2ogr', { verbosity: 0 }))
+    .pipe(map(function(contents) {
+      if(contents.toString().length === 0) {
+        throw new Error('ogr2ogr not found in path, install gdal.');
+      }
+    }));
+});
+
+gulp.task('verify:topojson', function() {
+  gulp.src('.')
+    .pipe(plumber())
+    .pipe(run('which topojson', { verbosity: 0 }))
+    .pipe(map(function(contents) {
+      if(contents.toString().length === 0) {
+        throw new Error('topojson not found in path, install topojson.');
+      }
+    }));
+});
+
+gulp.task('build', ['browserify', 'copy', 'build:map']);
 gulp.task('default', ['build', 'webserver', 'watch']);
--- src/js/index.js
+++ src/js/index.js
@@ -1,10 +1,18 @@
 'use strict';
 
+var _        = require('underscore');
 var d3       = require('d3');
 var topojson = require('topojson');
 
 var width  = 960;
 var height = 640;
+
+var projection = d3.geo.mercator()
+    .translate([width / 2, height / 2])
+    .scale((width - 1) / 2 / Math.PI);
+
+var path = d3.geo.path()
+    .projection(projection);
 
 var svg = d3
   .select('body')
@@ -12,14 +20,99 @@ var svg = d3
   .attr('width', width)
   .attr('height', height);
 
-d3.json('./assets/countries/countries.geo.json', function(err, countries) {
+// var scale = d3.scale.category20();
+
+var scale = d3.scale.linear()
+  .domain(Array.apply(null, {length: 29}).map(Number.call, Number))
+  .range([
+    '#ccece6',
+    '#99d8c9',
+    '#66c2a4',
+    '#41ae76',
+    '#238b45',
+    '#006d2c',
+    '#00441b',
+    '#d4b9da',
+    '#c994c7',
+    '#df65b0',
+    '#e7298a',
+    '#ce1256',
+    '#980043',
+    '#67001f',
+    '#fdd49e',
+    '#fdbb84',
+    '#fc8d59',
+    '#ef6548',
+    '#d7301f',
+    '#b30000',
+    '#7f0000',
+    '#d0d1e6',
+    '#a6bddb',
+    '#74a9cf',
+    '#3690c0',
+    '#0570b0',
+    '#045a8d',
+    '#023858',
+  ]);
+
+console.log(scale(0),scale(10),scale(20));
+
+var g = svg.append('g');
+
+var setNeighborDistance = function(element, distance) {
+  element.setAttribute('data-distance', distance);
+  element.setAttribute('fill', scale(distance));
+  element.setAttribute('stroke', scale(distance));
+
+  var neighbors = _.compact(element.getAttribute('data-borders').split(','));
+
+  _.each(neighbors, function(neighbor) {
+    var neighborElement = document.querySelector('.country[data-cca3=' + neighbor +']');
+
+    if(neighborElement === null) {
+      return;
+    }
+
+    var neighborDistance = parseInt(neighborElement.getAttribute('data-distance'));
+
+    if(!neighborDistance || distance + 1 < neighborDistance) {
+      setNeighborDistance(neighborElement, distance + 1);
+    }
+  });
+};
+
+d3.json('./assets/world/data.json', function(err, world) {
   if(err) { 
     return console.error(err);
   }
 
-  console.log(countries);
+  var countries = topojson.feature(world, world.objects.countries);
 
-  svg.append('path')
-    .datum(topojson.feature(countries, countries.features[131]))
-    .attr('d', d3.geo.path().projection(d3.geo.mercator()));
+  g.selectAll('.country')
+    .data(countries.features)
+    .enter().append('path')
+    .attr('class', 'country')
+    .attr('data-cca3', function(d) { return d.properties.cca3; })
+    .attr('data-name', function(d) { return d.properties.name; })
+    .attr('data-borders', function(d) { return d.properties.borders.join(','); })
+    .attr('d', path)
+    .on('mouseover', function(e) {
+      var elements = document.querySelectorAll('.country');
+      _.invoke(elements, 'removeAttribute', 'data-distance');
+      _.invoke(elements, 'removeAttribute', 'fill');
+      _.invoke(elements, 'setAttribute', 'stroke', '#fff');
+
+      setNeighborDistance(this, 1);
+
+      console.log('-----', e.properties.name, e.properties.cca3);
+      var max = 0;
+      _.each(elements, function(element) {
+        var distance = parseInt(element.getAttribute('data-distance'));
+        if(distance > max) {
+          max = distance;
+          console.log(element.getAttribute('data-name'), distance);
+        }
+      });
+      console.log('');
+    });
 });
